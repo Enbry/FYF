ce6cd4078f5000d86b30fdab74685ad1a03bd40c
noemie <noemybrocheray@gmail.com>
2016-02-19 15:23:02 +0100

    Initial Commit

---
+++ FYF/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FYF
+//
+//  Created by noemie on 19/02/2016.
+//  Copyright © 2016 all Touches. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
